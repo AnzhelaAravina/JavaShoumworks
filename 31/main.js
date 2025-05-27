d63b25ffc20bd8d1e16504b7cb08632c1c50f01b
AnzhelaAravina <desertyou16@gmail.com>
2025-05-27 11:57:58 +0300

    Add files via upload

---
+++ 31/main.js
@@ -0,0 +1,9 @@
+import {add, multiply} from "./math.js";
+console.log(add(2))
+console.log(multiply(4))
+
+import log from "./logger.js";
+console.log("Started")
+
+import {calculate as calc} from "./helpers.js"
+console.log(calc(2,5))
---
+++ 31/math.js
@@ -0,0 +1,7 @@
+export function add(a){
+    return a + a;
+}
+
+export function multiply(b){
+    return b * b
+}
